e2611624c13a49515c392f9283ffae1e26d9e1bd
icpcgu <145842724+icpcgu@users.noreply.github.com>
2023-09-23 14:05:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+const hourElement = document.querySelector(".hour");
+const minuteElement = document.querySelector(".minute");
+const secondElement = document.querySelector(".second");
+const dateElement = document.querySelector(".date");
+const timeElement = document.querySelector(".time");
+const toggleElement = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+toggleElement.addEventListener("click", () => {
+  document.querySelector("html").classList.toggle("dark");
+
+  if (toggleElement.textContent === "Dark mode") {
+    toggleElement.textContent = "Light mode";
+  } else {
+    toggleElement.textContent = "Dark mode";
+  }
+});
+
+const setTime = () => {
+  const time = new Date();
+  const date = time.getDate();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const AmPm = hours >= 12 ? "PM" : "AM";
+
+  hourElement.style.transform = `translate(-50%,-100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minuteElement.style.transform = `translate(-50%,-100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondElement.style.transform = `translate(-50%,-100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeElement.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${AmPm}`;
+
+  dateElement.innerHTML = `${days[day]} , ${months[month]} <span class='circle'>${date}</span>`;
+};
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+setTime();
+
+setInterval(setTime, 1000);
